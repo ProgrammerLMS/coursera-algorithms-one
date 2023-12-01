e342bdc7701b70bd5e7abfc57c53ce7e851a5d1d
LiMengSheng <927971135@qq.com>
2023-12-01 15:39:35 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# How to use algs4.jar in windows terminal?
+
+## 1. Put algs4.jar in somewhere
+
+- Such as, 
+
+  ```bash
+  C:\Users\username\algs4.jar
+  ```
+
+
+## 2. Use command like this 
+
+- ```bash
+  javac -cp C:\Users\username\algs4.jar T.java
+  ```
+
+  
+
+
+- ```bash
+  java -cp C:\Users\username\algs4.jar; T
+  ```
+
+  
+
--- lab0-hello/RandomWord.java
+++ lab0-hello/RandomWord.java
@@ -4,8 +4,21 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
     public static void main(String[] args) {
-
+        String champion = null;
+        int count = 1;
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / count)) {
+                champion = word;
+            }
+            count ++;
+        }
+        StdOut.println(champion);
     }
 }
